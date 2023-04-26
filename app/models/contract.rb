fe776ade73af08580505169e4fbf20dba8f63674
josefggx <josefgg58@gmail.com>
2023-04-25 23:53:47 -0500

    :sparkles: add: Improved the model logic

--- app/models/contract.rb
+++ app/models/contract.rb
@@ -1,13 +1,58 @@
 class Contract < ApplicationRecord
-  validates :job_title, presence: true
-  validates :contract_category, presence: true
-  validates :term, presence: true
-  validates :health_provider, presence: true
-  validates :risk_type, presence: true
-  validates :initial_date, presence: true
+  include DateConstants
+  include PayrollHelper
+
   validates :worker, presence: true
-  validates_associated :wages
+  validates :job_title, presence: true
+  validates :health_provider, presence: true
+  validates :term, inclusion: { in: CONTRACT_TERMS }
+  validates :risk_type, inclusion: { in: RISK_TYPES.keys }
+
+  validates :initial_date,
+            presence: { code: '0460' },
+            comparison: { greater_than: MIN_VALID_DATE, code: '0461', if: -> { initial_date.present? } }
+
+  validates :end_date,
+            date: { code: '0462' },
+            allow_blank: true,
+            comparison: { greater_than_or_equal_to: :initial_date, code: '0463',
+                          if: -> { initial_date.present? && end_date.present? } }
+
+  validate :validate_end_date_presence
 
   belongs_to :worker
-  has_many :wages, dependent: :destroy
+  has_many :wages, validate: true, dependent: :destroy
+
+  before_update :update_associated_salaries
+
+  def update_associated_salaries
+    oldest_salary = wages.order(initial_date: :asc).first
+    newest_salary = wages.order(initial_date: :desc).first
+    newest_base_salary = newest_salary&.base_salary
+    newest_transport_subsidiary = newest_salary&.transport_subsidy
+    oldest_base_salary = oldest_salary&.base_salary
+    oldest_transport_subsidiary = oldest_salary&.transport_subsidy
+
+    if end_date_changed? && end_date.nil? && !initial_date_changed?
+      newest_salary&.update_columns(end_date: end_date)
+    elsif end_date_changed? && !initial_date_changed? && (newest_salary&.end_date.nil? || newest_salary.end_date <= end_date)
+      newest_salary&.update_columns(end_date: end_date)
+    elsif initial_date_changed? && initial_date <= oldest_salary&.end_date
+      wages.destroy_all
+      wages.create(base_salary: oldest_base_salary, transport_subsidy:  oldest_transport_subsidiary,
+                   initial_date: initial_date, end_date: end_date)
+    else
+      wages.destroy_all
+      wages.create(base_salary: newest_base_salary, transport_subsidy:  newest_transport_subsidiary,
+                   initial_date: initial_date, end_date: end_date)
+    end
+  end
+
+  def validate_end_date_presence
+    if term == 'fixed' && end_date.blank?
+      errors.add(:end_date, :missing_end_date)
+    elsif term == 'indefinite' && end_date.present?
+      errors.add(:end_date, :unexpected_end_date)
+    end
+  end
 end
--- app/models/wage.rb
+++ app/models/wage.rb
@@ -1,7 +1,57 @@
 class Wage < ApplicationRecord
-  validates :base_salary, presence: true, numericality: { greater_than_or_equal_to: 0, code: "5002" }
-  validates :transport_subsidy, presence: true
-  validates :initial_date, presence: true
+  validates :base_salary, presence: true, numericality: { greater_than_or_equal_to: 0, code: '5002' }
+  validates :transport_subsidy, inclusion: { in: [true, false] }
+  validates :initial_date, presence: true, date: true
+
+  validate :initial_date_in_contract_dates
+  validate :initial_date_greater_than_last, if: -> { new_record? || changed? }
+  validate :first_wage_initial_date
 
   belongs_to :contract
+  has_one :company, through: :contract
+
+  before_create :set_wage_end_date, :update_last_wage_on_create
+  before_update :update_last_wage_on_update
+  after_destroy :update_last_wage_on_destroy
+
+  def set_wage_end_date
+    self.end_date = contract.end_date
+  end
+
+  def update_last_wage_on_destroy
+    previous_wage = contract.wages.order(end_date: :desc).first
+    previous_wage&.update_columns(end_date: contract.end_date)
+  end
+
+  def update_last_wage_on_create
+    previous_wage = contract.wages.order(end_date: :desc).first
+    previous_wage&.update_columns(end_date: initial_date - 1.day)
+  end
+
+  def update_last_wage_on_update
+    previous_wage = contract.wages.order(end_date: :desc).second
+    previous_wage&.update_columns(end_date: initial_date - 1.day)
+  end
+
+  def initial_date_in_contract_dates
+    return unless initial_date.is_a?(Date) && contract.end_date.present? && initial_date > contract.end_date
+
+    errors.add(:initial_date, :out_of_range)
+  end
+
+  def initial_date_greater_than_last
+    return unless initial_date.is_a?(Date) && !new_record?
+
+    last_wage_initial_date = contract.wages.order(initial_date: :desc).limit(1).pluck(:initial_date).first
+
+    return unless last_wage_initial_date.present? && initial_date < last_wage_initial_date
+
+    errors.add(:initial_date, :invalid)
+  end
+
+  def first_wage_initial_date
+    return unless !new_record? && initial_date > contract.initial_date && contract.wages.count == 1
+
+    errors.add(:initial_date, :out_of_range)
+  end
 end
