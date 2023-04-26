19d7f6a421d9d2d15d8373e0019fd7849b0c1459
josefggx <josefgg58@gmail.com>
2023-04-25 23:45:08 -0500

    :bug: fix: corrected migrations

--- db/migrate/20230420160907_create_workers.rb
+++ db/migrate/20230420160907_create_workers.rb
@@ -2,7 +2,7 @@ class CreateWorkers < ActiveRecord::Migration[7.0]
   def change
     create_table :workers, id: :uuid do |t|
       t.string :name, null: false
-      t.integer :id_number, limit: 10, null: false
+      t.integer :id_number, null: false
       t.references :company, null: false, foreign_key: true, type: :uuid
 
       t.timestamps
--- db/migrate/20230420194528_create_contracts.rb
+++ db/migrate/20230420194528_create_contracts.rb
@@ -1,11 +1,13 @@
 class CreateContracts < ActiveRecord::Migration[7.0]
   def change
+    create_enum :contract_terms, %w[fixed indefinite]
+    create_enum :risk_type, %w[risk_1 risk_2 risk_3 risk_4 risk_5]
+
     create_table :contracts, id: :uuid do |t|
       t.string :job_title, null: false
-      t.string :contract_category, null: false
-      t.string :term, null: false
+      t.enum :term, null: false, enum_type: 'contract_terms'
       t.string :health_provider, null: false
-      t.string :risk_type, null: false
+      t.enum :risk_type, null: false, enum_type: 'risk_type'
       t.date :initial_date, null: false
       t.date :end_date, null: true
       t.references :worker, null: false, foreign_key: true, type: :uuid, unique: true
