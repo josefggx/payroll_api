8e5f9529b443ad8e1e87fdbb6397cc6fd8abccc5
josefggx <josefgg58@gmail.com>
2023-04-30 11:47:02 -0500

    :sparkles: add: payroll updater service

---
+++ app/services/payroll_updater.rb
@@ -0,0 +1,30 @@
+class PayrollUpdater
+  def self.call(payroll)
+    new(payroll).update_payroll
+  end
+
+  def initialize(payroll)
+    @payroll = payroll
+  end
+
+  def update_payroll
+    payroll_fields = PayrollCalculator.call(worker, period, @payroll)
+    @payroll.update(**payroll_fields)
+
+    if @payroll.save
+      { success: true, payroll: @payroll}
+    else
+      { success: false, errors: @payroll.errors }
+    end
+  end
+
+  private
+
+  def period
+    @payroll.period
+  end
+
+  def worker
+    @payroll.worker
+  end
+end
